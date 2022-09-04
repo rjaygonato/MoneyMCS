ac003a0440e895b3260405b51b9ef05a8f0cd0b5
zvz23 <ziegfredzorrilla23@gmail.com>
2022-09-04 21:54:00 +0800

    tailwind postcss

---
+++ MoneyMCS/Pages/AddResource.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MoneyMCS.Pages
+{
+    public class AddResourceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
