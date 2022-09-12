42c75e752ba6fccf5ea2d83cf611e2d07a7faea8
zvz23 <ziegfredzorrilla23@gmail.com>
2022-09-12 23:37:25 +0800

    start agent page

---
+++ MoneyMCS/Pages/Agent/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MoneyMCS.Pages.Agent
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
