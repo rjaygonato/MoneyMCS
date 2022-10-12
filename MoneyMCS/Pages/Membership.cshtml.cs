b58babc21d1658b1ccb88279e250389957466c67
zvz23 <ziegfredzorrilla23@gmail.com>
2022-10-13 07:36:54 +0800

    added temp membership page

---
+++ MoneyMCS/Pages/Membership.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MoneyMCS.Pages
+{
+    public class MembershipModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
